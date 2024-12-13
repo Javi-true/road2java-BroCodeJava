26f703ed894ce2d3a073e7c7cb2e1338821ff49a
Javi-true <rivera.dev@outlook.com>
2024-12-13 03:32:01 -0600

    Method with two parameters

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
 
     //basic method.
-    static void hello(String name){
-        System.out.println("Hello " + name);
+    static void hello(String name, int num){
+        System.out.println("Hello " + name+ " tu numero es el "+ num);
     }
 
     //main method.
     public static void main(String[] args) {
 
         String name = "bro";
+        int num = 1;
 
-        hello(name);
+        hello(name, num);
 
     }
 }
