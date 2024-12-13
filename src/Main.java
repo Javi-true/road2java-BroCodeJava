22241dbeeb2eb15e4bb298e1c9f02790ebc4ac65
Javi-true <rivera.dev@outlook.com>
2024-12-13 04:03:39 -0600

    overloaded methods

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ public class Main {
         return num1 + num2;
     }
 
+    static int sumar(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    static double sumar(double num1, double num2, double num3){
+        return num1 + num2 + num3;
+    }
+
     //main method.
     public static void main(String[] args) {
-        int x = 3;
-        int y = 4;
 
-        System.out.println(sumar(x,y));
-
+        System.out.println(sumar(1,2));
+        System.out.println(sumar(1,2,3));
+        System.out.println(sumar(1.0,2.0,3.0));
     }
 }
