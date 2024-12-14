9da48081830f4e1651392d405605b59d3f556de6
Javier Trujillo <rivera.dev@outlook.com>
2024-12-14 02:24:40 -0600

    printf test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
 
-
+    public static void main(String[] args){
+        int num1 = 2;
+        String name = "Javier";
+        System.out.printf("hola %-9s, tu numero es %03d",name,2);
+    }
 }
