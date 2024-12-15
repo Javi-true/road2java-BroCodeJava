5229fcfd267b5f5adaba5ee632ba8bfc98743b38
Javier Trujillo <rivera.dev@outlook.com>
2024-12-14 22:28:18 -0600

    printf method practice basic concepts

--- src/PracPrintf.java
+++ src/PracPrintf.java
@@ -1,3 +1,14 @@
 public class PracPrintf {
-
+    public static void main(String[] args){
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Bro";
+        int myInt = 50;
+        double myDouble = 1000;
+        //System.out.printf("%b",myBoolean);
+        //System.out.printf("%c",myChar);
+        //System.out.printf("%s",myString);
+        //System.out.printf("%d",myInt);
+        System.out.printf("%.2f",myDouble);
+    }
 }
